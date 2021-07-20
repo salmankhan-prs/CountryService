a6d52d77ccf98c2f9eed55527f0e9a01a87b5ed7
Salman khan <salmankhanprs@gmail.com>
2021-07-20 15:37:33 +0530

    to aws

--- src/main/java/com/example/countryservice/CountryServiceApplication.java
+++ src/main/java/com/example/countryservice/CountryServiceApplication.java
@@ -4,13 +4,16 @@ import com.example.countryservice.ui.RequestModel;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.internal.util.Lists;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @SpringBootApplication
+@EnableSwagger2
 public class CountryServiceApplication {
 
     public static void main(String[] args) {
